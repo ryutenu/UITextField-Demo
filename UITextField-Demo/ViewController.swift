15d6d40181ed649b541244aa8255b527487475d4
Alan Liu <ryutenu.tokyo@gmail.com>
2022-02-22 20:52:48 +0900

    [Mod] Storyboard

--- UITextField-Demo/ViewController.swift
+++ UITextField-Demo/ViewController.swift
@@ -8,12 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
 }
-
