6980dcae2dfb0a3fe3cdbd6eeec81bbd819a8913
Alan Liu <ryutenu.tokyo@gmail.com>
2022-02-23 00:43:45 +0900

    [Mod] Minor fixes

--- UITextField-Demo/ViewController.swift
+++ UITextField-Demo/ViewController.swift
@@ -29,39 +29,39 @@ class ViewController: UIViewController {
         let gesture = UITapGestureRecognizer()
         view.addGestureRecognizer(gesture)
         
+        // 入力欄以外タッチするとキーボードが閉じる
         gesture.rx.event.asDriver().drive(onNext: { _ in
-            /// 入力欄以外タッチするとキーボードが閉じる
             self.view.endEditing(true)
         }).disposed(by: disposeBag)
         
+        // 入力するたびにこの処理が走る
         textField.rx.text.orEmpty.asDriver().drive(onNext: { [unowned self] text in
-            // 入力するたびにこの処理が走る
             label.text = text.isEmpty ? "label" : text
         }).disposed(by: disposeBag)
         
+        // キーボードが表示された時に処理が走る
         textField.rx.controlEvent(.editingDidBegin).asDriver().drive(onNext: { _ in
-            // キーボードが表示された時に処理が走る
             print("editingDidBegin")
         }).disposed(by: disposeBag)
         
+        // textFieldの値が変更されるたびに処理が走る
         textField.rx.controlEvent(.editingChanged).asDriver().drive(onNext: { _ in
-            // textFieldの値が変更されるたびに処理が走る
+//            print("editingChanged")
         }).disposed(by: disposeBag)
         
-        textField.rx.controlEvent(.editingDidEnd).asDriver().drive(onNext: { _ in
-            // キーボードが閉じた時に処理が走る
-            print("editingDidEnd")
-            // 入力した内容を保存する
-            UserDefaults.standard.set(self.textField.text, forKey: "text")
-        }).disposed(by: disposeBag)
-        
+        // キーボードのReturn Keyがタップされる時に処理が走る
         textField.rx.controlEvent(.editingDidEndOnExit).asDriver().drive(onNext: { _ in
-            // キーボードのReturn Keyがタッチされる時に処理が走る
             print("editingDidEndOnExit")
         }).disposed(by: disposeBag)
         
+        // キーボードが閉じた時に処理が走る
+        textField.rx.controlEvent(.editingDidEnd).asDriver().drive(onNext: { _ in
+            print("editingDidEnd")
+            UserDefaults.standard.set(self.textField.text, forKey: "text")
+        }).disposed(by: disposeBag)
+        
+        // 不明
         textField.rx.controlEvent(.valueChanged).asDriver().drive(onNext: { _ in
-            // 不明
             print("valueChanged")
         }).disposed(by: disposeBag)
     }
