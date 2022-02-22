874f7ba9769f4e59b91a2d0e40028237ecc3b7b2
Alan Liu <ryutenu.tokyo@gmail.com>
2022-02-23 00:37:15 +0900

    [Mod] Setup text field

--- UITextField-Demo/ViewController.swift
+++ UITextField-Demo/ViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
     
@@ -13,8 +15,54 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var textField: UITextField!
     
+    var disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        textField.text = UserDefaults.standard.string(forKey: "text")
+        
+        setupTextField()
+    }
+    
+    private func setupTextField() {
+        let gesture = UITapGestureRecognizer()
+        view.addGestureRecognizer(gesture)
+        
+        gesture.rx.event.asDriver().drive(onNext: { _ in
+            /// 入力欄以外タッチするとキーボードが閉じる
+            self.view.endEditing(true)
+        }).disposed(by: disposeBag)
+        
+        textField.rx.text.orEmpty.asDriver().drive(onNext: { [unowned self] text in
+            // 入力するたびにこの処理が走る
+            label.text = text.isEmpty ? "label" : text
+        }).disposed(by: disposeBag)
+        
+        textField.rx.controlEvent(.editingDidBegin).asDriver().drive(onNext: { _ in
+            // キーボードが表示された時に処理が走る
+            print("editingDidBegin")
+        }).disposed(by: disposeBag)
+        
+        textField.rx.controlEvent(.editingChanged).asDriver().drive(onNext: { _ in
+            // textFieldの値が変更されるたびに処理が走る
+        }).disposed(by: disposeBag)
+        
+        textField.rx.controlEvent(.editingDidEnd).asDriver().drive(onNext: { _ in
+            // キーボードが閉じた時に処理が走る
+            print("editingDidEnd")
+            // 入力した内容を保存する
+            UserDefaults.standard.set(self.textField.text, forKey: "text")
+        }).disposed(by: disposeBag)
+        
+        textField.rx.controlEvent(.editingDidEndOnExit).asDriver().drive(onNext: { _ in
+            // キーボードのReturn Keyがタッチされる時に処理が走る
+            print("editingDidEndOnExit")
+        }).disposed(by: disposeBag)
+        
+        textField.rx.controlEvent(.valueChanged).asDriver().drive(onNext: { _ in
+            // 不明
+            print("valueChanged")
+        }).disposed(by: disposeBag)
     }
 }
